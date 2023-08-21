d1c7ff1b37cc581ef70fdf46c94b88d3c2ef5483
Yens Van Hout <vanhoutyens@gmail.com>
2023-08-21 15:36:34 +0200

    More styling, create now redirects to new recipe

--- src/components/Recipe.vue
+++ src/components/Recipe.vue
@@ -5,37 +5,38 @@ const props = defineProps({
 </script>
 <template>
   <div v-if="props.Recipe?.title">
-    <h2 class="text-2xl text-center">{{ props.Recipe?.title }}</h2>
+    <h2 class="text-3xl text-center">{{ props.Recipe?.title }}</h2>
     <!-- <div class="image">
       <img
         src="https://static.ah.nl/static/recepten/img_RAM_PRD178796_1224x900_JPG.jpg"
         alt="Chili con carne"
       />
     </div> -->
-    <div id="recipeDetails" class="ml-6">
+    <div id="recipeDetails" class="ml-6 md:flex justify-evenly md:text-xl">
       <div id="ingredientList">
-        <h2>Ingredients:</h2>
-        <ul>
+        <h2 class="text-2xl">Ingredients:</h2>
+        <ul class="list-disc">
           <li v-for="ingredient in props.Recipe?.ingredients">
             {{ ingredient }}
           </li>
         </ul>
       </div>
-      <div id="stepList" class="mt-6">
-        <h2>Steps:</h2>
-        <ol>
+      <div id="stepList" class="mt-6 w-1/6 md:text-xl">
+        <h2 class="text-2xl">Steps:</h2>
+        <ol class="list-decimal">
           <li v-for="step in props.Recipe?.steps">
             {{ step }}
           </li>
         </ol>
       </div>
-      <div id="tagList" class="mt-6">
-        <span class="pill" v-for="tag in props.Recipe?.tags">
-          {{ tag }}
-        </span>
-      </div>
+    </div>
+    <div id="tagList" class="mt-6 flex justify-center">
+      <span class="pill" v-for="tag in props.Recipe?.tags">
+        {{ tag }}
+      </span>
     </div>
   </div>
+
   <div v-else>
     <h1 class="text-center text-3xl">Recipe not found</h1>
   </div>
